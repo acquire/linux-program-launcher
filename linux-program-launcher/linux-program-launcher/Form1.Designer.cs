a679d19874fc96e2e70167961fb1bc45ca188519
acquire <acquire@users.noreply.github.com>
2016-04-09 23:42:32 -0700

    Added files via upload

---
+++ linux-program-launcher/linux-program-launcher/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace linux_program_launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(8, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(264, 212);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 254);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "linux-program-launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ linux-program-launcher/linux-program-launcher/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace linux_program_launcher
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> items = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //start form in middle of screen
+            StartPosition = FormStartPosition.CenterScreen;
+
+            var files = Directory.GetFiles(@"/usr/bin");
+            foreach (var f in files)
+            {
+                items.Add(Path.GetFileName(f));
+            }
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(items.ToArray<string>());
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(textBox1.Text))
+                {
+                    listBox1.Items.Clear();
+                    listBox1.Items.AddRange(items.ToArray<string>());
+                }
+                else
+                {
+                    listBox1.Items.Clear();
+                    listBox1.Items.AddRange(items.Where(i => i.Contains(textBox1.Text)).ToArray<string>());
+                }
+                if(listBox1.Items.Count > 0)
+                {
+                    listBox1.SelectedIndex = 0;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                //launch the program
+                if (e.KeyCode == Keys.Enter)
+                {
+                    LaunchProgram();
+                    e.SuppressKeyPress = true;
+                    this.Close();
+                }
+
+                //move the selection up
+                if (e.KeyCode == Keys.Down)
+                {
+                    if (listBox1.SelectedIndex != listBox1.Items.Count - 1)
+                    {
+                        listBox1.SelectedIndex += 1;
+                    }
+                }
+
+                //move the selection down
+                if (e.KeyCode == Keys.Up)
+                {
+                    if (listBox1.SelectedIndex != 0)
+                    {
+                        listBox1.SelectedIndex -= 1;
+                    }
+                }
+
+                //press esc to exit
+                if (e.KeyCode == Keys.Escape)
+                {
+                    this.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void LaunchProgram()
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "setsid";
+            startInfo.Arguments = listBox1.SelectedItem.ToString();
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //launch the program
+            if (e.KeyCode == Keys.Enter)
+            {
+                LaunchProgram();
+                e.SuppressKeyPress = true;
+                this.Close();
+            }
+
+            //press esc to exit
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+
+            else
+            {
+                e.SuppressKeyPress = true;
+                textBox1.Focus();
+                textBox1.SelectionStart = textBox1.Text.Length;
+
+                //handle transition when focusing from listbox to textbox
+                if (e.KeyCode == Keys.Back)
+                {
+                    //when you press the backspace key on listbox
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                }
+                else
+                {
+                    //when you try to type something on the listbox
+                    textBox1.Text += e.KeyData.ToString().ToLower();
+                }
+                
+                textBox1.SelectionStart = textBox1.Text.Length;
+
+
+            }
+        }
+    }
+}
